a0b14318f4319400928ca28d1f470cd0c446b71a
Devdreamsolution <q3construction1@gmail.com>
2021-01-05 09:29:38 +1000

    setup default string length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 }
