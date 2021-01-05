d29bbc735485b2fbc2988a1329043163df6df11f
Devdreamsolution <q3construction1@gmail.com>
2021-01-06 00:49:52 +1000

    signup and login endpoints

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -11,17 +11,17 @@ use Validator;
 class AuthController extends BaseController
 {
     /**
-     * Register api
+     * Sign Up api
      *
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function signup(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
+            // 'name' => 'required',
             'email' => 'required|email',
             'password' => 'required',
-            'c_password' => 'required|same:password',
+            // 'c_password' => 'required|same:password',
         ]);
    
         if($validator->fails()){
@@ -30,11 +30,12 @@ class AuthController extends BaseController
    
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
+        $input['name'] = $input['email'];
         $user = User::create($input);
         $success['token'] =  $user->createToken('MyApp')->accessToken;
         $success['name'] =  $user->name;
    
-        return $this->sendResponse($success, 'User register successfully.');
+        return $this->sendResponse($success, 'User signup successfully.');
     }
    
     /**
@@ -45,7 +46,11 @@ class AuthController extends BaseController
     public function login(Request $request)
     {
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
-            $user = Auth::user(); 
+            $user = Auth::user();
+
+            if ($user->email_verified_at === null)
+                return $this->sendError('Email no verificated.', ['error'=>'Not email verification']);
+
             $success['token'] =  $user->createToken('MyApp')-> accessToken; 
             $success['name'] =  $user->name;
    
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register', [AuthController::class, 'register']);
+Route::post('signup', [AuthController::class, 'signup']);
 Route::post('login', [AuthController::class, 'login']);
      
 Route::middleware('auth:api')->group( function () {
