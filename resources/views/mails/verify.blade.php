051c4636a79c4e55e2549d3477004d78fc18a974
Devdreamsolution <q3construction1@gmail.com>
2021-01-06 00:50:10 +1000

    email verification template

---
+++ resources/views/mails/verify.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Demystifying Email Design</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+<style>
+	.take-me-there{ 
+		background-color: #ef4056;
+		border-radius: 2px;
+		color: #ffffff;
+		display: inline-block;
+		line-height: 40px;
+		text-align: center;
+		text-decoration: none;
+		width: 220px;
+		font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;
+	}
+</style>
+</head>
+<body style="margin: 0; padding: 0;">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%"> 
+        <tr>
+            <td style="padding: 10px 0 30px 0;">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc; border-collapse: collapse;">
+                    <tr>
+                        <td align="center" bgcolor="#70bbd9" style="padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;">
+                            WELCOME
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+	                            <tr>
+                                    <td style="padding: 20px 0 20px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+                                        <p>Welcome to Ugigs Stream!!</p>
+                                        <p>Thank you for signing up to Ugigs live streaming app for artists and audience members. If you are signing up as an artist you can start scheduling live performances via our amazing new app to a brand new audience right away!</p>
+													 <p>As an audience member, you can find your favourite genres of music or live performances to tune into LIVE!</p>
+													 <p>Please click on the link below to activate your account and complete your profile before getting stuck in!</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style="color: #153643; font-family: Arial, sans-serif; font-size: 14px; ">
+                                        <a href="<?php echo $url?>" class="take-me-there">Activation Link</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;">
+                                        <p>Thanks and ENJOY!</p>
+                                        <p>Ugigs Team</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td bgcolor="#eee" style="padding: 30px 30px 30px 30px;">
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                <tr>
+                                    <td style="color: #555; font-family: Arial, sans-serif; font-size: 14px;" width="75%">
+                                        &reg; Ugigs.com <?php echo date('Y'); ?><br/>
+                                    </td>
+                                    <td align="right" width="25%">
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                            <tr>
+                                                <td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+
+                                                </td>
+                                                <td style="font-size: 0; line-height: 0;" width="20">&nbsp;</td>
+                                                <td style="font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;">
+
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
