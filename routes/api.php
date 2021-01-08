ea26d0b6172265f6999f140deaebda73fb64e06a
Devdreamsolution <q3construction1@gmail.com>
2021-01-08 11:36:39 +1000

    Add expire time and some auth routes

--- routes/api.php
+++ routes/api.php
@@ -19,29 +19,29 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(['middleware' => ['throttle:60,1']], function () {
+    Route::post('login', [AuthController::class, 'postLogin']);
+    Route::post('signup', [AuthController::class, 'postSignup']);
+    Route::post('forgot-password', [AuthController::class, 'postForgotPassword']);
 
-Route::post('signup', [AuthController::class, 'postSignup']);
-Route::post('login', [AuthController::class, 'postLogin']);
+    Route::middleware('auth:api')->group( function () {
+        Route::get('user', [AuthController::class, 'getUser']);
+        Route::get('logout', [AuthController::class, 'getLogout']);
+        
+        Route::resource('products', ProductController::class);
 
-Route::post('forgot-password', [AuthController::class, 'postForgotPassword']);
-
-Route::middleware('auth:api')->group( function () {
-    Route::resource('products', ProductController::class);
-
-    Route::group(['prefix' => 'profile'], function () {
-        Route::get('detail/{profile}', [ProfileController::class, 'getDetail']);
-        Route::post('store', [ProfileController::class, 'postStore']);
-        Route::post('cover-photo', [ProfileController::class, 'postCoverPhoto']);
-        Route::post('profile-photo', [ProfileController::class, 'postProfilePhoto']);
+        Route::group(['prefix' => 'profile'], function () {
+            Route::get('detail/{profile}', [ProfileController::class, 'getDetail']);
+            Route::post('store', [ProfileController::class, 'postStore']);
+            Route::post('cover-photo', [ProfileController::class, 'postCoverPhoto']);
+            Route::post('profile-photo', [ProfileController::class, 'postProfilePhoto']);
+        });
     });
-});
 
-Route::fallback(function(){
-    return response()->json([
-        'success' => false,
-        'message' => 'Page Not Found. If error persists, contact info@website.com',
-    ], 404);
+    Route::fallback(function(){
+        return response()->json([
+            'success' => false,
+            'message' => 'Page Not Found. If error persists, contact info@website.com',
+        ], 404);
+    });
 });
